7785e895f8f1b5d1801dbbce183327852dc42d08
rgup63 <guptaritu2503@gmail.com>
2020-10-20 17:27:37 +0530

    Basic

---
+++ VSIXProject_WebView2_10674/VSIXProject2/ToolWindow1.cs
@@ -0,0 +1,34 @@
+﻿namespace VSIXProject2
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using Microsoft.VisualStudio.Shell;
+
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("87e23817-ceb3-4a06-ba30-1c30af09f059")]
+    public class ToolWindow1 : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ToolWindow1"/> class.
+        /// </summary>
+        public ToolWindow1() : base(null)
+        {
+            this.Caption = "WebViewToolWindow";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new ToolWindow1Control();
+        }
+    }
+}
---
+++ VSIXProject_WebView2_10674/VSIXProject2/ToolWindow1Control.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace VSIXProject2
+{
+    using System.Windows.Controls;
+    using Microsoft.Web.WebView2.Core;
+
+    /// <summary>
+    /// Interaction logic for ToolWindow1Control.
+    /// </summary>
+    public partial class ToolWindow1Control : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ToolWindow1Control"/> class.
+        /// </summary>
+        public ToolWindow1Control()
+        {
+            this.InitializeComponent();
+            InitializeAsync();
+        }
+
+        private async void InitializeAsync()
+        {
+            var env = await CoreWebView2Environment.CreateAsync(null, "C:\\temp");
+            await webView.EnsureCoreWebView2Async(env);
+        }
+    }
+}
